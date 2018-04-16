8dd2d6e7ddf9a2f55068ab28f8cdc92a63e90c2c
Rafael Furry <rfurry@gmail.com>
2018-04-16 15:25:44 -0700

    Update restaurant styling

--- src/containers/Restaurant/Restaurant.js
+++ src/containers/Restaurant/Restaurant.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Header, Segment, Grid } from 'semantic-ui-react';
+import ReactStars from 'react-stars';
 import { GoogleMap } from 'components';
 
 class Restaurant extends Component {
@@ -8,7 +9,7 @@ class Restaurant extends Component {
 
     this.state = {
       name: 'La Carta de Oaxaca',
-      address: 'La Carta de Oaxaca',
+      address: '5431 Ballard Ave NW, Seattle, WA 98107',
       phone: '+1 206-782-8722',
       website: 'http://www.lacartadeoaxaca.com/',
       rating: 4,
@@ -23,11 +24,25 @@ class Restaurant extends Component {
               <GoogleMap />
             </Grid.Column>
             <Grid.Column textAlign="left">
-              <Header>{this.state.name}</Header>
-              <p>{this.state.address}</p>
+              <Header style={{ marginBottom: '10px' }}>
+                {this.state.name}
+              </Header>
+              <ReactStars
+                count={5}
+                size={14}
+                value={this.state.rating}
+                color2="#ffd700"
+                edit={false}
+              />
+              <p style={{ marginTop: '10px' }}>{this.state.address}</p>
               <p>{this.state.phone}</p>
               <p>{this.state.website}</p>
-              <p>{this.state.rating}</p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+                aut dolore quam maxime nulla eum ullam enim voluptas dolorum
+                eius! Similique ea voluptate reiciendis quibusdam itaque.
+                Perferendis fugiat temporibus unde.
+              </p>
             </Grid.Column>
           </Grid>
         </Segment>
